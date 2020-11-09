fbd391ec8869bbb1c268c2a6c20653abdc142402
WillCurran <willecurran@gmail.com>
2020-11-09 14:32:13 -0600

    1-bit single XOR gate working

--- GMW/garbled_circuit.py
+++ GMW/garbled_circuit.py
@@ -1,52 +1,83 @@
 from enum import Enum
 ## A GateType is a possible gate function. 
 class GateType(Enum):
-    NULL = -1 # what do I need for this?
+    NULL = -1
     NOT = 0
     XOR = 1
     AND = 2
 
 ## A Wire is a directed edge from one gate to another, along which a value is carried.
 ## We will use the Wire class in the Circuit's adjacency list
-class Wire():
-    def __init__(self, _value, _source=None, _destination=None):
+class Wire:
+    # id_counter = 0
+
+    def __init__(self, _value=None, _source=None, _destination=None):
+        # self.id = id_counter
         self.value = _value
         self.source = _source
         self.destination = _destination
+        # Wire.id_counter += 1
 
 
 ## A Gate is an implementation of a logic gate, with inbound and outbound Wires.
-## Assume that id increases topologically from left to right in a circuit. Must create gates in increasing order.
-class Gate():
-    id_counter = 0
-
-    # return the gate function for this gate. Assumes 2 inputs max
-    def gateFunction(self):
+## Implemented as a Doubly-Linked Adjacency List?
+class Gate:
+    # return the gate function evaluation for this gate. Assumes 2 inputs max
+    def gate_function_eval(self):
         if self.type == GateType.NOT:
-            return lambda x, y : ~x     # careful, this assumes we have an unsigned integer value
+            return ~self.inbound_wires[0].value     # careful, this assumes we have an unsigned integer value
         elif self.type == GateType.XOR:
-            return lambda x, y : x ^ y
+            print("Wire vals:", self.inbound_wires[0].value, self.inbound_wires[1].value)
+            print("Computing XOR. Got", self.inbound_wires[0].value ^ self.inbound_wires[1].value)
+            return self.inbound_wires[0].value ^ self.inbound_wires[1].value
         elif self.type == GateType.AND: # TODO
-            return lambda x, y : x
+            return self.inbound_wires[0].value
+        return -1                                   # gate is of type NULL
 
     def __init__(self, _type=GateType.NULL):
-        self.id = id_counter
         self.type = _type
-        self.adjacency_list = []
-        Gate.id_counter += 1
-    
-    # if inbound wires have values, then set outbound wire values
-    def evalGate():
-        pass
+        self.inbound_wires = []
+        self.outbound_wires = []
+
+    # assume gates are in topological order, so inbound wires must have a value
+    def evalGate(self):
+        if self.type != GateType.NULL:
+            print("Gate Type:", self.type)
+            if self.type == GateType.NOT:
+                assert(self.inbound_wires[0].value != None) # debug
+            else:
+                assert(self.inbound_wires[0].value != None) # debug
+                assert(self.inbound_wires[1].value != None) # debug
+            gate_output = self.gate_function_eval()
+            for outbound_wire in self.outbound_wires:
+                outbound_wire.value = gate_output
 
 ## A GarbledCircuit is a data structure to represent a logical circuit. It is a topologically sorted graph.
 ## In this case, we support NOT, AND, and XOR gates.
-class GarbledCircuit():
-
+class GarbledCircuit:
     # evaluate a circuit from left to right, and return final wire values
-    def evaluateCircuit(self):
-        pass
+    # assumes initial wire values are set and gates are in topological order
+    ########### CONSIDER INPUT/OUTPUT GATE LABELS ##############
+    def evaluate_circuit(self):
+        print("Evaluating circuit...")
+        for gate in self.gates:
+            gate.evalGate()
+        return [wire.value for wire in self.gates[len(self.gates) - 1].inbound_wires] # assumes only 1 gate holds all of the output wires (false assumption)
 
     def __init__(self, _gates=[], _wires=[]):
         self.gates = _gates
         self.wires = _wires
+
+    def insertGate(self, _type=GateType.NULL):
+        new_gate = Gate(_type)
+        self.gates.append(new_gate)
+        return new_gate
+
+    def insertWire(self, _value=None, _source=None, _destination=None):
+        assert(_source in self.gates) # debug
+        assert(_destination in self.gates) # debug
+        new_wire = Wire(_value, _source, _destination)
+        self.wires.append(new_wire)
+        _source.outbound_wires.append(new_wire) # python immutability/pointer question. will this work for us?
+        _destination.inbound_wires.append(new_wire) # python immutability/pointer question. will this work for us?
+        return new_wire
--- GMW/gmw.py
+++ GMW/gmw.py
@@ -1,1 +1,56 @@
-# create a test circuit and run it
+from garbled_circuit import *
+import random
+import copy
+
+class Party:
+    def __init__(self, _gc, _input):
+        self.gc = _gc                           # my copy of the garbled circuit
+        self.input = _input                     # my input (1-bit total for now)
+        self.r = random.randint(0, 1)
+        self.xor_share = self.input ^ self.r
+        self.r_other = None                     # the random share of the other party's secret input
+
+# create a test circuit and run it across 2 parties with GMW
+def execGMW():
+    # A makes a gc
+    gc_A = GarbledCircuit()
+    init_gate_A = gc_A.insertGate()
+    init_gate_B = gc_A.insertGate()
+    xor_gate = gc_A.insertGate(GateType.XOR) # just an XOR gate to start
+    end_gate = gc_A.insertGate()
+
+    gc_A.insertWire(_source=init_gate_A, _destination=xor_gate)
+    gc_A.insertWire(_source=init_gate_B, _destination=xor_gate)
+    gc_A.insertWire(_source=xor_gate, _destination=end_gate)
+
+    # B makes an identical gc
+    gc_B = copy.deepcopy(gc_A)
+
+    alice_input = int(input("Alice input: ")[0])
+    bob_input = int(input("Bob input: ")[0])
+    # both parties own the same gc
+    alice = Party(gc_A, alice_input)
+    bob = Party(gc_B, bob_input)
+    # exchange shares
+    alice.r_other = bob.r
+    bob.r_other = alice.r
+
+    # Alice sets her wire values
+    alice.gc.wires[0].value = alice.xor_share
+    alice.gc.wires[1].value = alice.r_other
+    # Bob sets his wire values
+    bob.gc.wires[0].value = bob.xor_share
+    bob.gc.wires[1].value = bob.r_other
+    print("Alice shares:", alice.xor_share, ", ", alice.r_other)
+    print("Bob shares:", bob.xor_share, ", ", bob.r_other)
+
+    # both parties evaluate on their own with no communication (only an XOR gate)
+    res_A = alice.gc.evaluate_circuit()
+    res_B = bob.gc.evaluate_circuit()
+    print("Alice got", res_A)
+    print("Bob got", res_B)
+    # combine shares to compute result (reveal secret output) together
+    result = [res_A[i] ^ res_B[i] for i in range(len(res_A))]
+    print("Actual result:", result)
+
+execGMW()
