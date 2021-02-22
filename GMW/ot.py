a18d4d87ff4198ae271068d21dd1c3ffca984998
WillCurran <willecurran@gmail.com>
2021-02-22 15:15:29 -0600

    incomplete effort on ot, will return

--- GMW/ot.py
+++ GMW/ot.py
@@ -38,12 +38,12 @@ def send_pk(conn, N, e):
 # Bob has received Alice's keys and will now encrypt his choice
 # conn is an outbound side of a pipe open with Alice
 def send_choice_enc(conn, E, m, N):
-    # TODO - why is 190 bytes the max size? why not 255?
-    k = secrets.randbits(190*8).to_bytes(190, byteorder=sys.byteorder, signed=False)
-    # k = secrets.randbits(N).to_bytes(N//8, 'big', signed=False)
     c = secrets.randbelow(2)
+    # TODO - why is 190 bytes the max size? why not 255?
+    # k = secrets.randbits(190*8).to_bytes(190, byteorder=sys.byteorder, signed=False)
+    k = (1 ^ m[c]).to_bytes(190, byteorder=sys.byteorder, signed=False)
     pk = serialization.load_pem_public_key(E)
-    k_enc = pk.encrypt(
+    q = pk.encrypt(
         k,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -51,39 +51,15 @@ def send_choice_enc(conn, E, m, N):
             label=None
         )
     )
-    print("k_enc size:", sys.getsizeof(k_enc))
-    print("m[c] size:", sys.getsizeof(m[c]))
-    print("k_enc size to int:", sys.getsizeof(int.from_bytes(k_enc, byteorder=sys.byteorder, signed=False)))
-    q = int.from_bytes(k_enc, byteorder=sys.byteorder, signed=False) ^ m[c]
-    print("q size:", sys.getsizeof(q))
     conn.send(("q", q))
     return (c, k, pk)
 
 # Alice decrypts what bob has sent (two possibilities), then 
 # encrypts her bits with both possibilities and sends the encrypted result
 def send_bits_enc(conn, q, m0, m1, sk_alice, b0, b1, N):
-    # TODO - Here, does the reduction of message space compromise correctness as well?
-    print("q size:", sys.getsizeof(q))
-    cipher1 = (q ^ m0).to_bytes(N//8, byteorder=sys.byteorder, signed=False)
-    cipher2 = (q ^ m1).to_bytes(N//8, byteorder=sys.byteorder, signed=False)
-    print("m0 size:", sys.getsizeof(m0))
-    print("m1 size:", sys.getsizeof(m1))
-    print("cipher1 size:", sys.getsizeof(cipher1))
-    print("cipher2 size:", sys.getsizeof(cipher2))
-    k0 = int.from_bytes(
+    q_dec = int.from_bytes(
         bytes=sk_alice.decrypt(
-            cipher1,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()), 
-                algorithm=hashes.SHA256(), 
-                label=None
-            )
-        ),
-        byteorder=sys.byteorder
-    )
-    k1 = int.from_bytes(
-        bytes=sk_alice.decrypt(
-            cipher2,
+            q,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()), 
                 algorithm=hashes.SHA256(), 
@@ -93,16 +69,18 @@ def send_bits_enc(conn, q, m0, m1, sk_alice, b0, b1, N):
         byteorder=sys.byteorder
     )
     s = secrets.randbelow(2)
+    k0 = q_dec ^ m0
+    k1 = q_dec ^ m1
     m = None
     if s == 1:
-        m = [k1 ^ m0, k0 ^ m1]
+        m = [k1 ^ b0, k0 ^ b1]
     else:
-        m = [k0 ^ m0, k1 ^ m1]
+        m = [k0 ^ b0, k1 ^ b1]
     conn.send(("m, s", (m, s)))
 
 # Bob decrypts
 def decrypt_bit(m, c, s, k):
-    return m[s^c] ^ k
+    return m[s^c] ^ int.from_bytes(k, sys.byteorder, signed=False)
 
 def alice_exec_example_OT(conn, b0, b1, N, e):
     print("alice reporting for duty!")
