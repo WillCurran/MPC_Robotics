75c8c42d4e07484eb166f49b1934dc3a7982989b
croja022 <43307251+croja022@users.noreply.github.com>
2021-03-17 13:22:32 -0400

    Added less optimized version of together filter.
    Renamed old together filter "minimized".
    Fixed delta function indexes.

--- incremental_moore_JI/DFA_test.py
+++ incremental_moore_JI/DFA_test.py
@@ -9,20 +9,33 @@ def test():
         'outputs': [0b0000, 0b0001, 0b0010] # moore machine outputs. need to have some assumption of how many bits for garbling.
 		}
     together_filter = {'alphabet': [0,1,2,3], # 3 is null, null must be all 1s in bits
+        'states': 6, # or could represent as [0, 1, 2, ..., |Q|]
+        'initial': 0,
+        'delta': [(1,2,3,0), (0,3,2,1), (3,0,5,2), (2,4,0,3),(0,3,2,4),(0,3,2,5)], # index is which state. tuple contains the delta from that state if 0 or 1
+        'outputs': ['blue','red','blue','blue','red', 'red']#(01,10)(11,00)(20,02)(12,21)(11,22)(22,00)
+		}
+    binary_together_filter = {'alphabet': [0,1],
+        'states': 18, #each non-binary state needs two states per number of bits needed for alphabet
+        'initial': 0,
+        # index is which state. tuple contains the delta from that state if 0 or 1
+        'delta': [
+        (1,2),(3,6),(9,0),(4,5),(0,9),(6,3),(7,8),(9,0),(15,6),(10,11),(6,12),(0,9),(13,14),(0,9),(6,12),(16,17),(0,9),(6,15)],
+        'outputs': [0,2,2,1,2,2,0,2,2,0,2,2,1,2,2,1,2,2] # 0 is blue, 1 is red, 2 is bogus
+		}
+    minimal_together_filter = {'alphabet': [0,1,2,3], # 3 is null, null must be all 1s in bits
         'states': 4, # or could represent as [0, 1, 2, ..., |Q|]
         'initial': 0,
-        'terminals': [0,1,2,3] # not sure if this is necessary, all states are terminal states
         'delta': [(1,3,2,0), (0,2,3,1), (3,1,0,2), (2,0,1,3)], # index is which state. tuple contains the delta from that state if 0 or 1
         'outputs': ['blue','blue','blue','red'] # moore machine outputs. need to have some assumption of how many bits for garbling.
 		}
-    binary_together_filter = {'alphabet': [0,1],
+    minimal_binary_together_filter = {'alphabet': [0,1],
         'states': 12, #each non-binary state needs two states per number of bits needed for alphabet
         'initial': 0,
-        'terminals': [0,1,2,3,4,5,6,7,8,9,10,11]
         # index is which state. tuple contains the delta from that state if 0 or 1
-        'delta': [(1,2),(4,10),(7,0), (5,6),(0,7),(10,4), (8,9),(10,4),(0,7), (11,12),(7,0),(4,10)],
-        'outputs': [0,0,0,0,0,0,0,0,0,1,1,1] # 0 is blue, 1 is red
+        'delta': [(1,2),(3,9),(6,0), (4,5),(0,6),(9,3), (7,8),(9,3),(0,6), (10,11),(6,0),(3,9)],
+        'outputs': [0,2,2,0,2,2,0,2,2,1,2,2] # 0 is blue, 1 is red, 2 is bogus
 		}
+    dfa = minimal_binary_together_filter
     x_a = input("Alice's input: ")
     x_b = input("Bob's input: ")
     n = len(x_a)
@@ -35,50 +48,8 @@ def test():
 
     k = 4 # security parameter
     s = 8 # statistical security parameter
-    
-    alice = Alice(dfa, x_a, n, k, s) # Alice creates keypair
-    bob = Bob(x_b, dfa, alice.public_key, k, s) # Bob creates garbled matrix
-    alice.init_state_and_pad(bob.init_state, bob.init_pad)
 
-    print("M")
-    print_M(bob.M)
-    print("PER")
-    print_M(bob.PER)
-    print("PM")
-    print_M(bob.PM)
-    print("GM")
-    print_M(bob.GM)
-    for i in range(3):
-        print("Iteration", i)
-        bob.append_GM_row()
-        print("M")
-        print_M(bob.M)
-        print("PER")
-        print_M(bob.PER)
-        print("PM")
-        print_M(bob.PM)
-        print("GM")
-        print_M(bob.GM)
+    M = DfaMat(dfa,n)
+    print("Standard Eval:", evalDfa(M, n, xor_input, dfa['initial']))
     
-    print("Standard Eval:", evalDfa(bob.M, n, xor_input, dfa['initial']))
-    print("Permuted Eval:", evalDfa(bob.PM, n, xor_input, bob.PER[0][dfa['initial'] ]))
-    
-    print("Performing OT garbled key transfer...")
-    choices_enc = alice.encrypt_input_i()
-    strings_enc = bob.send_garbled_key(choices_enc, alice.row_i)
-    print("--------------------------------------------")
-    print("Garbled Eval:", alice.step3(strings_enc, bob.GM))
-    print("Performing OT garbled key transfer...")
-    choices_enc = alice.encrypt_input_i()
-    strings_enc = bob.send_garbled_key(choices_enc, alice.row_i)
-    print("Garbled Eval:", alice.step3(strings_enc, bob.GM))
-    print("Performing OT garbled key transfer...")
-    choices_enc = alice.encrypt_input_i()
-    strings_enc = bob.send_garbled_key(choices_enc, alice.row_i)
-    print("Garbled Eval:", alice.step3(strings_enc, bob.GM))
-    print("Performing OT garbled key transfer...")
-    choices_enc = alice.encrypt_input_i()
-    strings_enc = bob.send_garbled_key(choices_enc, alice.row_i)
-    print("Garbled Eval:", alice.step3(strings_enc, bob.GM))
-
 test()
